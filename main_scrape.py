648911f8854d388fb2b5525abc27ca575585d85b
husakki <hueschromo@gmail.com>
2022-02-25 19:47:01 +0100

    weird errors...
    I deleted something and now I get bugs.. python is windows is hard to setup

--- main_scrape.py
+++ main_scrape.py
@@ -1,5 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+
 def main():
-    print("start")
+    driver = webdriver.Chrome()
+    driver.get("https://scrolller.com/r/cats?sort=top&filter=pictures")
 
 
 if __name__ == "__main__":
