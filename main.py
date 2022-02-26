c8c820fb7f5e9db9429bfcde4addcbe6b07b9b01
husakki <hueschromo@gmail.com>
2022-02-26 12:20:01 +0100

    change the time.sleep to WebDriverWait

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ def main():
 
     # open the webpage
     driver.get("https://scrolller.com/r/cats?sort=top&filter=pictures")
-    time.sleep(5)  # forgot to wait.. TODO solve this better
-    element = driver.find_element(By.CLASS_NAME, 'vertical-view__columns')\
+    element = WebDriverWait(driver, 5).until(lambda x: x.find_element(By.CLASS_NAME, 'vertical-view__columns')) \
         .find_element(By.CLASS_NAME, 'vertical-view__item').find_element(By.CSS_SELECTOR, 'img.vertical-view__media')
 
     src = element.get_attribute('src')
