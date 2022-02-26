8c46ddb1111c0c787a6b89a2a0bf32d44ff82423
husakki <hueschromo@gmail.com>
2022-02-26 13:33:34 +0100

    started scraping :smiley:
    first scrape

--- main.py
+++ main.py
@@ -9,15 +9,35 @@ import time
 def main(url):
     driver = webdriver.Chrome()
     driver.get(url)
+    time.sleep(5)  # Temporary Solution because with the other one I get not code completion :x
 
-    element = WebDriverWait(driver, 5).until(lambda x: x.find_element(By.CLASS_NAME, 'vertical-view__columns')) \
-        .find_element(By.CLASS_NAME, 'vertical-view__item').find_element(By.CSS_SELECTOR, 'img.vertical-view__media')
+    # element = WebDriverWait(driver, 5).until(lambda x: x.find_element(By.CLASS_NAME, 'vertical-view__columns').
+    #                                          find_element(By.CLASS_NAME, 'vertical-view__item').
+    #                                          find_element(By.CSS_SELECTOR, 'img.vertical-view__media'))
 
-    src = element.get_attribute('src')
-    print(src)
+    # Getting the columns(generally 2-3)
+    elements = []
+    elements = driver.find_elements(By.CLASS_NAME, 'vertical-view__column')
+
+    # Getting the div where the images are located
+    div_elements = []
+    for x in elements:
+        tmp = x.find_elements(By.CLASS_NAME, 'vertical-view__item')
+        div_elements.append(tmp)
+
+    # flatting the list
+    flat_div_elements = [item for sublist in div_elements for item in sublist]
+    
+    # Getting the image URL's
+    image_url = []
+    for y in flat_div_elements:
+        tmp = y.find_element(By.CSS_SELECTOR, 'img.vertical-view__media')
+        image_url.append(tmp.get_attribute('src'))
+
+    print(image_url)
+
     driver.close()
 
 
 if __name__ == '__main__':
     main("https://scrolller.com/r/cats?sort=top&filter=pictures")
-
