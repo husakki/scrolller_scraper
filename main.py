358fcbba8bdbcd47da51f439d5577af4f0ccf97e
husakki <hueschromo@gmail.com>
2022-02-26 12:30:33 +0100

    minor changes

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 
-def main():
+def main(url):
     driver = webdriver.Chrome()
+    driver.get(url)
 
-    # open the webpage
-    driver.get("https://scrolller.com/r/cats?sort=top&filter=pictures")
     element = WebDriverWait(driver, 5).until(lambda x: x.find_element(By.CLASS_NAME, 'vertical-view__columns')) \
         .find_element(By.CLASS_NAME, 'vertical-view__item').find_element(By.CSS_SELECTOR, 'img.vertical-view__media')
 
     src = element.get_attribute('src')
     print(src)
+    driver.close()
 
 
 if __name__ == '__main__':
-    main()
+    main("https://scrolller.com/r/cats?sort=top&filter=pictures")
+
