7daaf903131ca0006093078b448e66a9911f8756
husakki <hueschromo@gmail.com>
2022-02-26 11:06:20 +0100

    getting one image
    was stuck at this problem for a long time. forgot to wait for the page to load.

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def main():
 
     # open the webpage
     driver.get("https://scrolller.com/r/cats?sort=top&filter=pictures")
-    elem = driver.find_element(By.CLASS_NAME, "gallery-view")
-    print(elem.text)
+    time.sleep(5)  # forgot to wait..
+    element = driver.find_element(By.CLASS_NAME, 'vertical-view__columns')\
+        .find_element(By.CLASS_NAME, 'vertical-view__item').find_element(By.CSS_SELECTOR, 'img.vertical-view__media')
+
+    src = element.get_attribute('src')
+    print(src)
 
 
 if __name__ == '__main__':
