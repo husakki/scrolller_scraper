373a98ef13c5595fca35aef460973f225b480e15
husakki <hueschromo@gmail.com>
2022-02-26 13:51:47 +0100

    printf and image_amount for later

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 
-def main(url):
+def main(url, image_amount):
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(5)  # Temporary Solution because with the other one I get not code completion :x
@@ -18,7 +18,7 @@ def main(url):
     # Getting the columns(generally 2-3)
     elements = []
     elements = driver.find_elements(By.CLASS_NAME, 'vertical-view__column')
-
+    print(f"elements_size: {len(elements)}")
     # Getting the div where the images are located
     div_elements = []
     for x in elements:
@@ -27,17 +27,17 @@ def main(url):
 
     # flatting the list
     flat_div_elements = [item for sublist in div_elements for item in sublist]
-    
+    print(f"flat_div_size: {len(flat_div_elements)}")
     # Getting the image URL's
     image_url = []
     for y in flat_div_elements:
         tmp = y.find_element(By.CSS_SELECTOR, 'img.vertical-view__media')
         image_url.append(tmp.get_attribute('src'))
 
-    print(image_url)
+    print(f"image_url_size: {len(image_url)} \n {image_url}")
 
     driver.close()
 
 
 if __name__ == '__main__':
-    main("https://scrolller.com/r/cats?sort=top&filter=pictures")
+    main("https://scrolller.com/r/cats?sort=top&filter=pictures", 10)
