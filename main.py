21d34222892870bff1c1c6705a9f87ae984abe05
husakki <hueschromo@gmail.com>
2022-02-27 10:05:02 +0100

    clean up

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 import time
 import urllib.request
 import re
@@ -13,16 +10,12 @@ def main(url, input_image_amount, path=None):
         path = "./images/"
     driver = webdriver.Chrome()
     driver.get(url)
-    time.sleep(6)  # Temporary Solution because with the other one I get not code completion :x
-
-    # element = WebDriverWait(driver, 5).until(lambda x: x.find_element(By.CLASS_NAME, 'vertical-view__columns').
-    #                                          find_element(By.CLASS_NAME, 'vertical-view__item').
-    #                                          find_element(By.CSS_SELECTOR, 'img.vertical-view__media'))
+    time.sleep(6)
 
     # Getting the columns(generally 2-3)
     elements = []
     elements = driver.find_elements(By.CLASS_NAME, 'vertical-view__column')
-    print(f"elements_size: {len(elements)}")
+    # print(f"elements_size: {len(elements)}")
     image_url = []
 
     while len(image_url) < input_image_amount:
@@ -34,8 +27,6 @@ def main(url, input_image_amount, path=None):
 
         time.sleep(5)  # Wait for page to load
 
-        # Todo scroll down
-        # Todo continue if same image already found
         # Getting the div where the images are located
         div_elements = []
         for x in elements:
@@ -44,19 +35,18 @@ def main(url, input_image_amount, path=None):
 
         # flatting the list
         flat_div_elements = [item for sublist in div_elements for item in sublist]
-        print(f"flat_div_size: {len(flat_div_elements)}")
+        # print(f"flat_div_size: {len(flat_div_elements)}")
+
         # Getting the image URL's
         url_finder(flat_div_elements, image_url, input_image_amount)
 
-    print(f"image_url_size: {len(image_url)} \n {image_url}")
+    # print(f"image_url_size: {len(image_url)} \n {image_url}")
 
     driver.close()
 
     counter = 1
     scrolller_name = re.search("r/([a-zA-Z0-9]+)[?]", url).group(1)
     for x in image_url:
-        # urllib.request.urlretrieve(x, scrolller_name + " " + str(counter) + ".jpg")
-
         urllib.request.urlretrieve(x, path + scrolller_name + " " + str(counter) + ".jpg")
         counter += 1
 
